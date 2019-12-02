Minor bug fixes - Clear lists before each calculation

calculationHelper.py
@@ -16,17 +16,17 @@ def getProbabilityBasedOnDistributionType(variable = Variable):
         return getSingleProbability(variableParameters)
 
 def getUniformProbability(parameters):
-    return np.random.uniform(low=parameters.Minimum, high=parameters.Maximum)
+    return random.uniform(parameters.Minimum, parameters.Maximum)
 
 def getTriangularProbability(parameters):
-    return np.random.triangular(left=parameters.Minimum, mode=parameters.MostLikely, right=parameters.Maximum)
+    return random.triangular(low=parameters.Minimum, high=parameters.Maximum, mode=parameters.MostLikely)
 
 def getSingleProbability(parameters):
     return parameters.FixedValue
 
 def calculateReserve(unitSystem = "", area = 0.0, thickness = 0.0, porosity = 0.0, waterSaturation = 0.0, recoveryFactor = 0.0, formationVolume = 0.0):
     recoverableOil = 0.0 #Np
-    reserveConst = 7758
+    reserveConst = 7758.0000
 
     numerator = area*thickness*porosity*(1 - waterSaturation)
     denominator = formationVolume